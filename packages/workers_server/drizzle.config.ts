db480f2f80cd3ebc0268411070fb67a878997202
Maxim Kopecki <kopecki.maxim@gmail.com>
2025-02-07 22:18:41 +0100

    db change fixes

--- packages/workers_server/drizzle.config.ts
+++ packages/workers_server/drizzle.config.ts
@@ -1,11 +1,10 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
-  dialect: 'sqlite',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.DB_URL!,
   },
 });
