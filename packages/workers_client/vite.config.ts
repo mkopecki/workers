0694d1fa5bd5b67bea624d2724f1665c9e7c6027
Maxim Kopecki <kopecki.maxim@gmail.com>
2025-04-24 19:11:04 +0200

    close plugin

--- packages/workers_client/vite.config.ts
+++ packages/workers_client/vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import {ClosePlugin} from "./vite_plugin_close";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react()],
+    plugins: [react(), ClosePlugin()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
