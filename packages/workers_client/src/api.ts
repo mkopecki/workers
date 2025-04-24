1caf075549e1823d5fd1da0b0e9ad2033cae0442
Maxim Kopecki <kopecki.maxim@gmail.com>
2025-04-24 19:19:53 +0200

    updates

--- packages/workers_client/src/api.ts
+++ packages/workers_client/src/api.ts
@@ -1,2 +1,2 @@
 export const build_server_url = (endpoint: string) =>
-  `${__SERVER_HOST__}${endpoint}`;
+  `${import.meta.env.__SERVER_HOST__}${endpoint}`;
--- packages/workers_client/vite.config.ts
+++ packages/workers_client/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import {ClosePlugin} from "./vite_plugin_close";
+import { ClosePlugin } from "./vite_plugin_close";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
